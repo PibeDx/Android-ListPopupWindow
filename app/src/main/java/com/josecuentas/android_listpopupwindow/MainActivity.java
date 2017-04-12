e414f10d5bc523618380c1a4a3f16886831747cd
José Cuentas Turpo <jcuentast@gmail.com>
2017-04-12 16:32:28 -0500

    refactor: changes for bug

--- app/src/main/java/com/josecuentas/android_listpopupwindow/MainActivity.java
+++ app/src/main/java/com/josecuentas/android_listpopupwindow/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ListPopupWindow;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -26,7 +27,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         popupWindow();
         mEteProductName.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
+                listPopupWindow.setInputMethodMode(listPopupWindow.isShowing() ? ListPopupWindow.INPUT_METHOD_NEEDED : ListPopupWindow.INPUT_METHOD_NOT_NEEDED);
+
                 listPopupWindow.show();
+                listPopupWindow.getListView().setOverScrollMode(View.OVER_SCROLL_ALWAYS);
             }
         });
 
@@ -38,19 +42,40 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     private void popupWindow() {
-        listPopupWindow = new ListPopupWindow(this);
+        /*listPopupWindow = new ListPopupWindow(this);
         listPopupWindow.setAdapter(new ArrayAdapter(this, R.layout.list_item, products));
         listPopupWindow.setAnchorView(mEteProductName);
         listPopupWindow.setWidth(mEteProductName.getMeasuredWidth());
         listPopupWindow.setHeight(400);
 
         listPopupWindow.setModal(true);
-        listPopupWindow.setOnItemClickListener(this);
+        listPopupWindow.setOnItemClickListener(this);*/
+        //
+        listPopupWindow = new ListPopupWindow(this);
+        listPopupWindow.setAdapter(new ArrayAdapter(this, R.layout.list_item, products));
+        listPopupWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        listPopupWindow.setAnchorView(mEteProductName);
+        listPopupWindow.setOnItemClickListener(new DropDownItemClickListener());
+        listPopupWindow.setModal(true);
 
+        // For dropdown width, the developer can specify a specific width, or
+        // MATCH_PARENT (for full screen width), or WRAP_CONTENT (to match the
+        // width of the anchored view).
+
+        listPopupWindow.setWidth(mEteProductName.getMeasuredWidth());
+        listPopupWindow.setHeight(400);
     }
 
     @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         mEteProductName.setText(products[position]);
         listPopupWindow.dismiss();
+    }
+
+    private class DropDownItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            mEteProductName.setText(products[position]);
+            listPopupWindow.dismiss();
+        }
     }
 }
